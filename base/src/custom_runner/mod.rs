d593c4fe82bbe83f4733dd65d4592dffaaf2333c
SomeSuperCoder <ithelper9@gmail.com>
2024-03-21 14:06:55 +0300

    Actually execute user code

--- base/src/custom_runner/mod.rs
+++ base/src/custom_runner/mod.rs
@@ -4,6 +4,10 @@ use crate::instruction::Instruction;
 use borsh::BorshDeserialize;
 use sc_rpc::SCRPC;
 use crate::program_result::ProgramResult;
+use tokio::time::timeout;
+use tokio;
+use mlua::{Lua, LuaOptions, StdLib};
+use mlua::prelude::LuaError;
 
 pub struct CustomRunner {}
 
@@ -15,6 +19,27 @@ impl CustomRunner {
             let program_result = ProgramResult::default();
 
             if let Ok(user_code) = String::from_utf8(instruction.program_account.data) {
+                let result = tokio::runtime::Runtime::new().unwrap().block_on(
+                    timeout(std::time::Duration::from_secs(3), execute_user_code(user_code))
+                );
+
+                if let Ok(result2) = result {
+                    match result2 {
+                        Err(lua_error) => {
+                            let lua_error_string = lua_error.to_string();
+                            println!("User code failed with error: {}", lua_error_string);
+                            
+                            return Err("User code failed")
+                        },
+                        _ => {
+
+                        }
+                    }
+                } else {
+                    println!("User code ran out of time");
+
+                    return Err("User code ran out of time")
+                }
                 println!("Executing user code!")
             } else {
                 println!("Failed to load user code. UTF8 decode error");
@@ -27,4 +52,10 @@ impl CustomRunner {
             Err("Unable to load data")
         }
     }
+}
+
+async fn execute_user_code(code: String) -> Result<(), LuaError> {
+    let lua = Lua::new_with(StdLib::NONE, LuaOptions::new()).unwrap();
+
+    lua.load(code).exec()
 }
